49caa2c6954fb82cbaa0100e3c7e53c3096bdc70
Cleber Azeredo <115503053+itscleber@users.noreply.github.com>
2025-06-28 21:05:34 -0300

    logger: separate loggers per level

--- config/logger.go
+++ config/logger.go
@@ -5,28 +5,32 @@ import (
 	"os"
 )
 
-var logger *log.Logger
+var (
+	infoLogger  *log.Logger
+	errorLogger *log.Logger
+	debugLogger *log.Logger
+	fatalLogger *log.Logger
+)
 
 func SetupLogger() {
-	logger = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
+	infoLogger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	errorLogger = log.New(os.Stdout, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	debugLogger = log.New(os.Stdout, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
+	fatalLogger = log.New(os.Stdout, "FATAL: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 func Info(message string) {
-	logger.SetPrefix("INFO: ")
-	logger.Println(message)
+	infoLogger.Println(message)
 }
 
 func Error(message string) {
-	logger.SetPrefix("ERROR: ")
-	logger.Println(message)
+	errorLogger.Println(message)
 }
 
 func Debug(message string) {
-	logger.SetPrefix("DEBUG: ")
-	logger.Println(message)
+	debugLogger.Println(message)
 }
 
 func Fatal(message string) {
-	logger.SetPrefix("FATAL: ")
-	logger.Fatalln(message)
+	fatalLogger.Fatalln(message)
 }
